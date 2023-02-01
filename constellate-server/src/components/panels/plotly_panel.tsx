03064ed37518e20efc3620e56d77c02fbd70257f
Nicholas Miklaucic <nicholas.miklaucic@gmail.com>
2023-02-01 13:59:40 -0500

    Remove MathJax
    This is way too big for such a niche feature.

--- constellate-server/src/components/panels/plotly_panel.tsx
+++ constellate-server/src/components/panels/plotly_panel.tsx
@@ -72,7 +72,6 @@ export default function PlotlyPanel(props) {
 
     return (
         <>
-            <Script src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-AMS-MML_SVG" />
             <EuiTabbedContent
                 tabs={tabs}
                 initialSelectedTab={tabs[0]}
